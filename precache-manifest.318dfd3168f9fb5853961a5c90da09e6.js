b689ded08dc283d12343aaa0e3477d49342e9c94
Jeden1102 <bum123@o2.pl>
2022-08-10 23:03:24 +0200

    deploy

---
+++ precache-manifest.318dfd3168f9fb5853961a5c90da09e6.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421c1d469dda076c869c",
+    "url": "/css/app.0425291f.css"
+  },
+  {
+    "revision": "2d6370abfe19d8135257",
+    "url": "/css/chunk-vendors.1da7d742.css"
+  },
+  {
+    "revision": "a2f9cfd7895ccd91446d52392e2954ab",
+    "url": "/img/acc1.a2f9cfd7.png"
+  },
+  {
+    "revision": "1b2a616b2e0e52afa102a1920d5c917b",
+    "url": "/img/acc2.1b2a616b.png"
+  },
+  {
+    "revision": "596b0e31c526881dc451b1c1a779e3f2",
+    "url": "/img/acc3.596b0e31.png"
+  },
+  {
+    "revision": "2a1f86534d3c7d4b979ce2697d0d95d1",
+    "url": "/img/acc4.2a1f8653.png"
+  },
+  {
+    "revision": "c12217bed7fb59700bc4dc5bbf8f039d",
+    "url": "/img/acc5.c12217be.png"
+  },
+  {
+    "revision": "be50366f766436b7a8d6a28c716a6f21",
+    "url": "/img/dress1.be50366f.png"
+  },
+  {
+    "revision": "23b57db2820df0d1134592bc8b09ccf9",
+    "url": "/img/dress2.23b57db2.png"
+  },
+  {
+    "revision": "eceb6db8c11c663725c99e387fd1e47c",
+    "url": "/img/dress3.eceb6db8.png"
+  },
+  {
+    "revision": "2d59a283722f718b6dfe5ff80bf3af9c",
+    "url": "/img/dress4.2d59a283.png"
+  },
+  {
+    "revision": "af87ef8f59bc1aabe0d495042d7ba524",
+    "url": "/img/hat1.af87ef8f.png"
+  },
+  {
+    "revision": "7df535204aa93893f397dd625c995a92",
+    "url": "/img/hat2.7df53520.png"
+  },
+  {
+    "revision": "76e8b37d801abc36cd27c7bbff230bcb",
+    "url": "/img/hat3.76e8b37d.png"
+  },
+  {
+    "revision": "dc51fa65baf0846c6ef5bc832e7f085d",
+    "url": "/img/hat4.dc51fa65.png"
+  },
+  {
+    "revision": "085b7ca5415506c9bff71ec6b5ad1384",
+    "url": "/img/hat5.085b7ca5.png"
+  },
+  {
+    "revision": "2609cb6b17345d2a9a8a40609725b49c",
+    "url": "/img/hoodie1.2609cb6b.png"
+  },
+  {
+    "revision": "2d0e854fa565bab1af4ee49459c6dd6e",
+    "url": "/img/hoodie2.2d0e854f.png"
+  },
+  {
+    "revision": "019ac3154c96ed9d16ffe5fc70f347f5",
+    "url": "/img/hoodie3.019ac315.png"
+  },
+  {
+    "revision": "a3681d70401dd8546029be1ea6318d12",
+    "url": "/img/hoodie4.a3681d70.png"
+  },
+  {
+    "revision": "46fe22d55ba9d8925f723d014db4be92",
+    "url": "/img/hoodie5.46fe22d5.png"
+  },
+  {
+    "revision": "a71a4ea04cb15e9acc63e1b6ee187408",
+    "url": "/img/shoe1.a71a4ea0.png"
+  },
+  {
+    "revision": "e63a82cd5d62a499a1339d65ef53ee2f",
+    "url": "/img/shoe2.e63a82cd.png"
+  },
+  {
+    "revision": "143acad31cf85a14ef740fe1fa53f02d",
+    "url": "/img/shoe3.143acad3.png"
+  },
+  {
+    "revision": "f836c6550beccb2c00f37ebf33b7e7c8",
+    "url": "/img/shoe4.f836c655.png"
+  },
+  {
+    "revision": "e02bae4249df2edaf37c3d9ea75fd402",
+    "url": "/img/shoe5.e02bae42.png"
+  },
+  {
+    "revision": "0f411cddbb60753d9ce72e19af25a9c6",
+    "url": "/img/shoe6.0f411cdd.png"
+  },
+  {
+    "revision": "3964d6a636dde917dc84e98acda3159d",
+    "url": "/img/tr1.3964d6a6.png"
+  },
+  {
+    "revision": "2023373b4e929be89464d9f70bda0431",
+    "url": "/img/tr2.2023373b.png"
+  },
+  {
+    "revision": "0f2ed0000e2416ff2b4b44cd8784f97c",
+    "url": "/img/tr3.0f2ed000.png"
+  },
+  {
+    "revision": "2625ee9e39bbfa8ad687f036394c0adf",
+    "url": "/img/tr4.2625ee9e.png"
+  },
+  {
+    "revision": "79b073355e3a1e19d6cdda47f72897a6",
+    "url": "/img/tr5.79b07335.png"
+  },
+  {
+    "revision": "b3bea5e3e4a7d0bebe67df59813d9a18",
+    "url": "/img/trousers.b3bea5e3.png"
+  },
+  {
+    "revision": "876d72632267ee1e51e1f703effc7d4a",
+    "url": "/img/tshirt1.876d7263.png"
+  },
+  {
+    "revision": "d8cda24d2d2439bace9b478a57924c39",
+    "url": "/img/tshirt2.d8cda24d.png"
+  },
+  {
+    "revision": "9bba3be18679fbaa170843af826073a4",
+    "url": "/img/tshirt3.9bba3be1.png"
+  },
+  {
+    "revision": "7213f096d7a9231667608730106146c4",
+    "url": "/img/tshirt4.7213f096.png"
+  },
+  {
+    "revision": "16d6a6650625affa8bee57e3c8f557c5",
+    "url": "/img/tshirt5.16d6a665.png"
+  },
+  {
+    "revision": "aa5979fe06332206d059aece90c2a325",
+    "url": "/img/tshirt6.aa5979fe.png"
+  },
+  {
+    "revision": "08585337d583592cd50eb89e7c03d258",
+    "url": "/img/tshirt7.08585337.png"
+  },
+  {
+    "revision": "1454658e71175e12c43bfcc5d6582c41",
+    "url": "/img/un1.1454658e.png"
+  },
+  {
+    "revision": "74def2a76ea0dddcd8211bc4596c9f7b",
+    "url": "/img/un2.74def2a7.png"
+  },
+  {
+    "revision": "71e642a54b486e4cbb6f9a4da0333a00",
+    "url": "/img/un3.71e642a5.png"
+  },
+  {
+    "revision": "0436b05a6290118f2351ef5acc2082c3",
+    "url": "/img/un4.0436b05a.png"
+  },
+  {
+    "revision": "8c245588d9b77cf87d88f3587c67fedf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea66a511250a2db979a4",
+    "url": "/js/about.1ddad64f.js"
+  },
+  {
+    "revision": "421c1d469dda076c869c",
+    "url": "/js/app.55577a9d.js"
+  },
+  {
+    "revision": "2d6370abfe19d8135257",
+    "url": "/js/chunk-vendors.63ac7e7c.js"
+  },
+  {
+    "revision": "833d8182528a24c31b14f9eb0af10af2",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
